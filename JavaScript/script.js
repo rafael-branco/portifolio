ac1314ed022d06de98f4bdce062fd78dc2233dac
rafael-branco <rafael.rbranco@hotmail.com>
2024-01-12 12:40:00 -0300

    Update what was in host

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,8 +4,6 @@ let myAge = document.querySelector("#my-age");
 
 myAge.innerHTML = currentAge();
 
-
-
 function currentAge() {
     let myBirthday = new Date("1998-06-23");
     let currentDate = new Date();
