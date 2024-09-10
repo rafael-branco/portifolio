4b46c6d32e7e1a3a07e0e202b414904b4d79f97e
Rafael <rafael.rbranco@hotmail.com>
2024-09-10 00:05:08 -0300

    Add experiences, skills, and education.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,13 +8,14 @@ function currentAge() {
     let myBirthday = new Date("1998-06-23");
     let currentDate = new Date();
 
-    let differenceDate = currentDate - myBirthday;
+    let years = currentDate.getFullYear() - myBirthday.getFullYear();
 
-    var day = 1000 * 60 * 60 * 24;
-
-    let days = Math.floor(differenceDate / day);
-    let months = Math.floor(days / 31);
-    let years = Math.floor(months / 12);
+    if (
+        currentDate.getMonth() < myBirthday.getMonth() ||
+        (currentDate.getMonth() === myBirthday.getMonth() && currentDate.getDate() < myBirthday.getDate())
+    ) {
+        years--;
+    }
 
     return years;
 }
